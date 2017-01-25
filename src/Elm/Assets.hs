58b18d27b1274f0fda7c911a8379f644fc29b43d
Evan Czaplicki <evancz@users.noreply.github.com>
2017-01-25 15:37:28 -0800

    Have a central file for managing Elm assets on disk

---
+++ src/Elm/Assets.hs
@@ -0,0 +1,57 @@
+{-# OPTIONS_GHC -Wall #-}
+module Elm.Assets
+  ( configPath
+  , getPackageRoot
+  , getReplRoot
+  , getMakeRoot
+  )
+  where
+
+import qualified System.Directory as Dir
+import qualified System.Environment as Env
+import System.FilePath ((</>))
+
+import qualified Elm.Compiler.Version as Version
+import qualified Elm.Package as Pkg
+
+
+
+-- PATHS
+
+
+configPath :: FilePath
+configPath =
+  "elm.json"
+
+
+
+-- GET ROOTS
+
+
+getPackageRoot :: IO FilePath
+getPackageRoot =
+  getRoot "package"
+
+
+getReplRoot :: IO FilePath
+getReplRoot =
+  getRoot "repl"
+
+
+getMakeRoot :: IO FilePath
+getMakeRoot =
+  getRoot "make"
+
+
+getRoot :: FilePath -> IO FilePath
+getRoot projectName =
+  do  maybeHome <- Env.lookupEnv "ELM_HOME"
+      home <- maybe (Dir.getAppUserDataDirectory "elm") return maybeHome
+      let root = home </> version </> projectName
+      Dir.createDirectoryIfMissing True root
+      return root
+
+
+version :: FilePath
+version =
+  Pkg.versionToString Version.version
