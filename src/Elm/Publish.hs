ff76fa760d8ab72a3e4f45b13e140e22c33ddd66
Evan Czaplicki <evancz@users.noreply.github.com>
2017-06-27 15:18:57 -0700

    Get Elm.Publish code compiling
    Then new logic checks that the code on GitHub matches what you have
    locally.

--- src/Elm/Publish.hs
+++ src/Elm/Publish.hs
@@ -3,27 +3,41 @@ module Elm.Publish (publish) where
 
 
 import Control.Monad (when)
+import Control.Monad.Except (catchError)
+import Control.Monad.Trans (liftIO)
+import qualified Data.Text as Text
+import qualified System.Directory as Dir
+import qualified System.Exit as Exit
+import qualified System.Process as Process
 
-import qualified Bump
-import qualified Deps.Website as Websites
-import qualified CommandLine.Helpers as Cmd
-import qualified Docs
-import qualified Elm.Docs as Docs
-import qualified Elm.Package.Description as Desc
+import qualified Deps.Website as Website
+import qualified Elm.Bump as Bump
 import qualified Elm.Package as Pkg
-import qualified GitHub
+import qualified Elm.Project as Project
+import qualified Elm.Project.Json as Project
+import qualified Elm.Project.Summary as Summary
+import qualified File.Args as Args
+import qualified File.Artifacts as Artifacts
+import qualified File.Compile as Compile
+import qualified File.Crawl as Crawl
+import qualified File.Plan as Plan
 import qualified Reporting.Error as Error
+import qualified Reporting.Progress as Progress
 import qualified Reporting.Task as Task
+import qualified Stuff.Paths as Path
 
 
 
-publish :: Summary -> Task.Task ()
-publish (Summary _ project _ _ _) =
+-- PUBLISH
+
+
+publish :: Summary.Summary -> Task.Task ()
+publish (Summary.Summary root project _ _ _) =
   case project of
     Project.App _ _ ->
       Task.throw Error.CannotPublishApp
 
-    Project.Pkg info@(Project.PkgInfo name summary _ version exposed _ _ _ _ _) ->
+    Project.Pkg info@(Project.PkgInfo name summary _ version exposed _ _ _ _) ->
       do
           Task.report (Progress.PublishStart name version)
 
@@ -32,20 +46,77 @@ publish (Summary _ project _ _ _) =
 
           Bump.validate root info
           commitHash <- verifyTag name version
+          verifyNoChanges commitHash
+          zipHash <- verifyZip root name version
 
-          Website.register name version commitHash
+          Website.register name version commitHash zipHash
 
           Task.report Progress.PublishEnd
 
 
-isSummary :: Text -> Bool
+isSummary :: Text.Text -> Bool
 isSummary summary =
   error "TODO check summary is not default, not empty, etc."
 
 
 verifyTag :: Pkg.Name -> Pkg.Version -> Task.Task String
 verifyTag name version =
-  do  publicVersions <- GitHub.getVersionTags name
-      if elem version publicVersions
-        then return ()
-        else Task.throw (Error.MissingTag version)
+  phase (Progress.CheckTag version) $
+    Website.githubCommit name version `catchError` \_ ->
+      Task.throw (Error.MissingTag version)
+
+
+verifyNoChanges :: String -> Task.Task ()
+verifyNoChanges commitHash =
+  phase Progress.CheckChanges $
+    do  -- https://stackoverflow.com/questions/3878624/how-do-i-programmatically-determine-if-there-are-uncommited-changes
+        exitCode <- liftIO $
+          Process.rawSystem "git" [ "diff-index", "--quiet", commitHash, "--" ]
+
+        case exitCode of
+          Exit.ExitSuccess ->
+            return ()
+
+          Exit.ExitFailure _ ->
+            Task.throw (error "TODO local modules do not match")
+
+
+verifyZip :: FilePath -> Pkg.Name -> Pkg.Version -> Task.Task Website.Sha
+verifyZip root name version =
+  do  hash <- phase Progress.CheckDownload $
+        Website.githubDownload name version
+
+      phase Progress.CheckBuild $ inPrepublishDir $
+        do  summary@(Summary.Summary _ project _ _ _) <- Project.getRoot
+            args <- Args.fromSummary summary
+            graph <- Crawl.crawl summary args
+            (dirty, cachedIfaces) <- Plan.plan summary graph
+            answers <- Compile.compile project cachedIfaces dirty
+            results <- Artifacts.ignore answers
+            Artifacts.writeDocs root results
+
+      return hash
+
+
+inPrepublishDir :: Task.Task a -> Task.Task a
+inPrepublishDir task =
+  do  runner <- Task.getSilentRunner
+      result <- liftIO $ Dir.withCurrentDirectory Path.prepublishDir (runner task)
+      either Task.throw return result
+
+
+
+-- PHASE REPORTING
+
+
+phase :: Progress.PublishPhase -> Task.Task a -> Task.Task a
+phase publishPhase task =
+  do  Task.report $ Progress.PublishProgress publishPhase Nothing
+
+      result <- task `catchError` \err ->
+        do  Task.report $ Progress.PublishProgress publishPhase (Just Progress.Bad)
+            Task.throw err
+
+      Task.report $ Progress.PublishProgress publishPhase (Just Progress.Good)
+
+      return result
